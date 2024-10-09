a09a57099d3a73fdcfcf9c3e714c582893784a3b
Mallifs <mallifs@icloud.com>
2024-10-09 10:27:46 +0300

    deployment update

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-8(j_(gb-4cxp3sz$o@1qq6-4^e4i7r9x+nwqit=xgw=_r%@ami
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+  
 ]
 
 ROOT_URLCONF = 'blog_project.urls'
